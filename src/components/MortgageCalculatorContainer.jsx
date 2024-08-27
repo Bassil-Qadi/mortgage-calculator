92c531c8d76c2fcb2f01fb3e310db136d81ae803
root <bassilalqadi.ba@gmail.com>
2024-08-27 12:30:18 +0300

    change label property [class] to [className]

--- src/components/MortgageCalculatorContainer.jsx
+++ src/components/MortgageCalculatorContainer.jsx
@@ -64,13 +64,13 @@ export default function MortgageCalculatorContainer() {
             >
               Mortgage Term
             </label>
-            <div class="mt-2">
+            <div className="mt-2">
               <input
                 type="text"
                 name="first-name"
                 id="first-name"
                 value={mortgageTerm}
-                autocomplete="given-name"
+                autoComplete="given-name"
                 onChange={(e) => setMortgageTerm(e.target.value)}
                 className="block w-full bg-transparent rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -84,13 +84,13 @@ export default function MortgageCalculatorContainer() {
             >
               Interest Rate
             </label>
-            <div class="mt-2">
+            <div className="mt-2">
               <input
                 type="text"
                 name="last-name"
                 id="last-name"
                 value={interestrate}
-                autocomplete="family-name"
+                autoComplete="family-name"
                 onChange={(e) => setInterestRate(e.target.value / 100)}
                 className="block w-full bg-transparent rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
